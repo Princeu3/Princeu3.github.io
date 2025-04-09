88da494a272eaefa46dbea8313a396044aaf5d78
Princeu3 <princeupadhyay1401@gmail.com>
2025-04-08 22:45:20 -0500

    UI Final

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,33 +91,6 @@ skillHeaderArr.forEach((element, idx) => {
   });
 });
 
-// QUALIFICATION TABS
-let education = document.getElementById("education");
-let work = document.getElementById("work");
-let educationheader = document.getElementById("educationheader");
-let workheader = document.getElementById("workheader");
-workheader.style.color = "var(--text-color)";
-educationheader.style.color = "var(--first-color)";
-
-educationheader.addEventListener("click", () => {
-  let condition1 = work.classList.contains("qualification-inactive");
-  if (!condition1) {
-    education.classList.remove("qualification-inactive");
-    work.classList.add("qualification-inactive");
-    workheader.style.color = "var(--text-color)";
-    educationheader.style.color = "var(--first-color)";
-  }
-});
-workheader.addEventListener("click", () => {
-  let condition2 = education.classList.contains("qualification-inactive");
-  if (!condition2) {
-    work.classList.remove("qualification-inactive");
-    education.classList.add("qualification-inactive");
-    educationheader.style.color = "var(--text-color)";
-    workheader.style.color = "var(--first-color)";
-  }
-});
-
 // PORTFOLIO SWIPER
 let swiper = new Swiper(".mySwiper", {
   cssMode: true,
